c80fe996ef1b4cbb2e697ff3e6767623d1c4ac06
Pnaveen <naveenviki13@gmail.com>
2025-03-06 10:04:14 +0530

    portfolio implementation

---
+++ script.js
@@ -0,0 +1,42 @@
+const dot = document.getElementById("cursorDot");
+
+document.addEventListener("mousemove", (event) => {
+    dot.style.left = `${event.pageX}px`;  // pageX accounts for scroll position
+    dot.style.top = `${event.pageY}px`;   // pageY accounts for scroll position
+});
+
+function openLinkedIn() {
+    window.open("https://www.linkedin.com/in/naveen-prabhu-926159190");
+}
+document.addEventListener("DOMContentLoaded", function () {
+    let javaProficiency = 75; // Percentage of proficiency
+    let progressBar = document.getElementById("java-progress");
+    let percentageText = document.getElementById("java-percentage");
+
+    // Animate progress bar
+    setTimeout(() => {
+        progressBar.style.width = javaProficiency + "%";
+        percentageText.textContent = javaProficiency + "%";
+    }, 500);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    let CProficiency = 70; // Percentage of proficiency
+    let progressBar = document.getElementById("C#-progress");
+    let percentageText = document.getElementById("C#-percentage");
+
+    // Animate progress bar
+    setTimeout(() => {
+        progressBar.style.width = CProficiency + "%";
+        percentageText.textContent = CProficiency + "%";
+    }, 500);
+});
+document.addEventListener("DOMContentLoaded",function(){
+    let HTMLProficiency = 85; // Percentage of proficiency
+    let progressBar = document.getElementById("HTML-progress");
+    let percentageText = document.getElementById("HTML-percentage");
+    setTimeout(()=>{
+        progressBar.style.width=HTMLProficiency +"%";
+        percentageText.textContent=HTMLProficiency + "%";
+    })
+}) 
+// <!-- BEM : Block Element Modifier B__E--M -->
